0ea626b4dc7b2c0cb8c1093fcab8bbb7a8c06290
CodeWithNab <codewithnab@gmail.com>
2024-03-15 15:29:43 +0530

    multiply numbers

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int num =20;
+
+        for (int i = 0; i <=10 ; i++) {
+            System.out.println(num+" * "+i+" = "+num*i);
+        }
+    }
+}
